2149ccbe9db05162200b909a25fbc0d0cb41dcc6
IKnowImNasty <33733983+IKnowImNasty@users.noreply.github.com>
2020-04-28 15:17:43 +0200

    Updated file

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 use AutomaticWordpress\Configurate;
+use AutomaticWordpress\Database;
+use AutomaticWordpress\Wordpress;
 
 define( 'ABS', __DIR__ );
 
@@ -8,19 +10,20 @@ require __DIR__.'/source/autoload.php';
 require __DIR__.'/zone/index.php';
 
 $wordpress_name = $argv[ 0 ] ?? uniqid( 'wordpress_' );
+$wordpress_template_location = __DIR__.'/source/wp-config-template.txt';
 
 $configurate = new Configurate( __DIR__.'/enviroment.ini' );
 
 if( !download_wordpress( $configurate ) )
     exit( 'Error! Cannot download wordpress' );
 
-$wordpress_file = __DIR__.'/zone/wordpress/';
+$wordpress_dir = __DIR__.'/zone/wordpress/';
 
-$database = new Database( $configurate );
+$database = new Database( $configurate, $wordpress_name );
 
-$wordpress = new Wordpress( $wordpress_file );
+$wordpress = new Wordpress( $configurate, $wordpress_template_location );
 $wordpress->add_database_property( $database );
-$wordpress->write();
+$wordpress->write( $wordpress_dir );
 
 if( !move_to_desination( $configurate, $wordpress_name ) )
     exit( "Error! Cannot move to destination location! \n Wordpress directory is avaliable in '$wordpress_file'" );
--- source/class/wordpress.class.php
+++ source/class/wordpress.class.php
@@ -3,15 +3,63 @@
 namespace AutomaticWordpress;
 
 final class Wordpress {
-    public function __construct( string $dir_location ) {
+    private $template       = null;
+    private $configuration  = null;
+    private $database       = null;
 
+    public function __construct( Configurate $configuration, string $template_loaction ) {
+        $this->configuration = $configuration;
+        $this->get_template_content( $template_loaction );
     }
 
     public function add_database_property( Database $database ) : bool {
-
+        if( $database == null ) return false;
+        $this->database = $database;
+        return true;
     }
 
-    public function write() : bool {
+    public function write( string $dir_location ) : bool {
+        file_put_contents( $dir_location.'wp-config.php', $this->generate() );
+        return false;
+    }
 
+    private function generate() : string {
+        $output = str_replace( [
+            '{database-name}',
+            '{database-user}',
+            '{database-password}',
+            '{database-host}',
+            '{database-port}',
+            '{salt-block}',
+            '{ftp-block}'
+        ], [
+            $this->database->name,
+            $this->database->user,
+            $this->database->password,
+            $this->database->host,
+            $this->database->port,
+            $this->get_salt_block(),
+            $this->get_ftp_block()
+        ], $this->template );
+        return $output;
+    }
+
+    private function get_template_content( string $template_loaction ) : void {
+        $this->template = file_get_contents( $template_loaction );
+    }
+
+    private function get_ftp_block() : string {
+        if( $this->configuration->ftp[ 'include_to_wordpress' ] ?? false ) {
+            if( !isset( $this->configuration->ftp[ 'host' ], $this->configuration->ftp[ 'username' ], $this->configuration->ftp[ 'password' ] ) )
+                exit( 'Error! FTP Configuration is not valid' );
+            $host = $this->configuration->ftp[ 'host' ];
+            $username = $this->configuration->ftp[ 'username' ];
+            $password = $this->configuration->ftp[ 'password' ];
+            return "define('FTP_USER', '$username');\ndefine('FTP_PASS', '$password');\ndefine('FTP_HOST', '$host');\n";
+        }else return '';
+    }
+
+    private function get_salt_block() : string {
+        return file_get_contents( 'https://api.wordpress.org/secret-key/1.1/salt/' );
     }
 }
