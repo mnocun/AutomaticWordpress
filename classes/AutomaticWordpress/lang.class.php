3eefafbb94c4a5d039016ac7c647b3e3b8cd4d61
Mateusz Nocun <mateusz.nocun@fingoweb.com>
2020-11-06 22:26:26 +0100

    Creating an executable environment

--- classes/AutomaticWordpress/lang.class.php
+++ classes/AutomaticWordpress/lang.class.php
@@ -12,14 +12,25 @@ class Lang
     const ES = 'es';
     const RU = 'ru';
 
+    protected static $langCodes = [
+        Lang::EN => '',
+        Lang::PL => 'pl_PL',
+        Lang::DE => 'de_DE',
+        Lang::ES => 'es_ES',
+        Lang::RU => 'ru_RU'
+    ];
+
     public static function getCode(string $lang) : string
     {
-        return [
-            Lang::EN => '',
-            Lang::PL => 'pl_PL',
-            Lang::DE => 'de_DE',
-            Lang::ES => 'es_ES',
-            Lang::RU => 'ru_RU'
-        ][$lang] ?? '';
+        return self::$langCodes[$lang] ?? Lang::EN;
+    }
+
+    public static function resolveLang($lang) : string
+    {
+        if (is_null($lang) || !in_array($lang, array_keys(self::$langCodes))) {
+            return Lang::EN;
+        }else {
+            return $lang;
+        }
     }
 }
